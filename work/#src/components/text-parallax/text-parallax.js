193e7102fec69d424c223728a832c86d34b385db
Spacespire <aleksandra@spacespire.com>
2023-05-16 16:40:34 +0300

    Trello tasks completed

--- work/#src/components/text-parallax/text-parallax.js
+++ work/#src/components/text-parallax/text-parallax.js
@@ -1,18 +1,15 @@
 {
     let textParallaxSection = document.querySelector('[data-text-parallax]');
-    if(textParallaxSection) {
-        let content = textParallaxSection.querySelector('.text-parallax__body');
+    if (textParallaxSection) {
+        let cover = textParallaxSection.querySelector('.text-parallax__cover');
+        let body = textParallaxSection.querySelector('.text-parallax__body');
 
-        gsap.to(content,{
-            y:`-${content.clientHeight - textParallaxSection.clientHeight + (textParallaxSection.clientHeight / 4)}`,
-            duration: 1,
-            //ease: 'none',
-            scrollTrigger: {
-                trigger: textParallaxSection,
-                start: 'top center',
-                end: 'bottom center',
-                scrub: 2,
-            }
-        })
+        ScrollTrigger.create({
+            trigger: cover,
+            pin: cover,
+            start: () => `top top`,
+            end: () => `+=${body.offsetHeight - cover.offsetHeight}`,
+            pinSpacing: false
+        });
     }
 }
