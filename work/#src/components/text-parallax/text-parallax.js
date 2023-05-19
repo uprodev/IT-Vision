ff24af6c680a16eddac6765311d68ee98395e332
Spacespire <aleksandra@spacespire.com>
2023-05-19 16:37:10 +0300

    MVP-development page added

--- work/#src/components/text-parallax/text-parallax.js
+++ work/#src/components/text-parallax/text-parallax.js
@@ -1,26 +1,28 @@
 {
-    let textParallaxSection = document.querySelector('[data-text-parallax]');
-    if (textParallaxSection) {
-        let cover = textParallaxSection.querySelector('.text-parallax__cover');
-        let body = textParallaxSection.querySelector('.text-parallax__body');
-        let bg = textParallaxSection.querySelector('.text-parallax__bg');
-
-        ScrollTrigger.create({
-            trigger: cover,
-            pin: cover,
-            start: () => `top top`,
-            end: () => `+=${body.offsetHeight - cover.offsetHeight}`,
-            pinSpacing: false
-        });
-
-        if(bg) {
+    let textParallaxSections = document.querySelectorAll('[data-text-parallax]');
+    if (textParallaxSections) {
+        textParallaxSections.forEach(textParallaxSection => {
+            let cover = textParallaxSection.querySelector('.text-parallax__cover');
+            let body = textParallaxSection.querySelector('.text-parallax__body');
+            let bg = textParallaxSection.querySelector('.text-parallax__bg');
+    
             ScrollTrigger.create({
-                trigger: bg,
-                pin: bg,
+                trigger: cover,
+                pin: cover,
                 start: () => `top top`,
-                end: () => `+=${body.offsetHeight - bg.offsetHeight}`,
+                end: () => `+=${body.offsetHeight - cover.offsetHeight}`,
                 pinSpacing: false
             });
-        }
+    
+            if(bg) {
+                ScrollTrigger.create({
+                    trigger: bg,
+                    pin: bg,
+                    start: () => `top top`,
+                    end: () => `+=${body.offsetHeight - bg.offsetHeight}`,
+                    pinSpacing: false
+                });
+            }
+        });
     }
 }
